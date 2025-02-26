a045389bfc7399cf3dd310c073dfad3051ad8033
apurbagoon1 <apurbagoon1@gmail.com>
2025-02-26 10:19:27 +0600

    added practice tasks of this module

---
+++ Practice/task-4.js
@@ -0,0 +1,24 @@
+/**
+ * Task-4
+Count the number of properties.
+
+Input:
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+
+Output: 4
+ */
+
+let student = {
+    name: "Apurba Goon",
+    age: 24,
+    city: "Gazipur",
+    isStudent: true
+};
+
+console.log(Object.keys(student).length); 
---
+++ Practice/task-5.js
@@ -0,0 +1,34 @@
+/**
+ * Task-5 (Hard)
+Loop through an object and print the key-value pairs with their types
+
+Input:
+
+let myObject = {
+
+name: 'John Doe',
+age: 25,
+city: 'Example City',
+isStudent: true
+};
+
+Output:
+
+key: name | type:  string
+key: age | type:  number
+key: city | type:  string
+key: isStudent | type:  boolean 
+ */
+
+let myObject = {
+    name: "Apurba Goon",
+    age: 24,
+    city: "Gazipur",
+    isStudent: true
+};
+
+for (let key in myObject) {
+    console.log("key:", key, "| type:", typeof myObject[key]);
+}
+
+
